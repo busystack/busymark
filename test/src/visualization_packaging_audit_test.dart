92776d8881a2ac72f9fad82a761991e408f8cb33
Albert G <albert.gee.ca@gmail.com>
2026-08-31 17:48:23 -0700

    Release/v0.3.3 (#29)
    Correct the release metadata to v0.3.3 after PR #28.

--- test/src/visualization_packaging_audit_test.dart
+++ test/src/visualization_packaging_audit_test.dart
@@ -11,10 +11,10 @@ void main() {
       'linux/io.busystack.busymark.metainfo.xml',
     ).readAsStringSync();
 
-    expect(pubspec, contains(RegExp(r'^version: 0\.3\.2$', multiLine: true)));
+    expect(pubspec, contains(RegExp(r'^version: 0\.3\.3$', multiLine: true)));
     expect(
       snapcraft,
-      contains(RegExp(r'^version: "0\.3\.2"$', multiLine: true)),
+      contains(RegExp(r'^version: "0\.3\.3"$', multiLine: true)),
     );
     expect(snapcraft, contains(RegExp(r'^grade: stable$', multiLine: true)));
     expect(
@@ -39,9 +39,9 @@ void main() {
         ),
       ),
     );
-    expect(metainfo, contains('<release version="0.3.2"'));
-    expect(pubspec, isNot(contains('0.3.24')));
-    expect(snapcraft, isNot(contains('0.3.24')));
+    expect(metainfo, contains('<release version="0.3.3"'));
+    expect(pubspec, isNot(contains('0.3.34')));
+    expect(snapcraft, isNot(contains('0.3.34')));
   });
 
   test(
