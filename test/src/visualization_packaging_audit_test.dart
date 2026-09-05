fb3a2155911fe5687ba4f890b682d75a07754ecb
albert <albert.gee.ca@gmail.com>
2026-09-04 21:29:37 -0700

    Bump version to 0.3.5

--- test/src/visualization_packaging_audit_test.dart
+++ test/src/visualization_packaging_audit_test.dart
@@ -11,10 +11,10 @@ void main() {
       'linux/io.busystack.busymark.metainfo.xml',
     ).readAsStringSync();
 
-    expect(pubspec, contains(RegExp(r'^version: 0\.3\.4$', multiLine: true)));
+    expect(pubspec, contains(RegExp(r'^version: 0\.3\.5$', multiLine: true)));
     expect(
       snapcraft,
-      contains(RegExp(r'^version: "0\.3\.4"$', multiLine: true)),
+      contains(RegExp(r'^version: "0\.3\.5"$', multiLine: true)),
     );
     expect(snapcraft, contains(RegExp(r'^grade: stable$', multiLine: true)));
     expect(
@@ -39,9 +39,9 @@ void main() {
         ),
       ),
     );
-    expect(metainfo, contains('<release version="0.3.4"'));
-    expect(pubspec, isNot(contains('0.3.44')));
-    expect(snapcraft, isNot(contains('0.3.44')));
+    expect(metainfo, contains('<release version="0.3.5"'));
+    expect(pubspec, isNot(contains('0.3.55')));
+    expect(snapcraft, isNot(contains('0.3.55')));
   });
 
   test(
